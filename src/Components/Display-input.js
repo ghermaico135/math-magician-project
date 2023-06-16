752d85f7d9a8707fe44d2db4babf13882a651a39
Miki <ghermaico135@gmail.com>
2023-06-16 16:33:41 +0300

    Display opeation

--- src/Components/Display-input.js
+++ src/Components/Display-input.js
@@ -4,28 +4,33 @@ import React from 'react';
 import './Style.css';
 
 function DisplayInput(props) {
+let obj=props.result
 
-  const handleClick = (e) => {
-    if(e.target.innerText == "Ac"){
-      props.setObj({
-        total: null,
-        next: null,
-        operation: null
+  if (obj === undefined || obj === null) {
+    return null; // or return an appropriate fallback component
+  } else {
+    const show = (obj) => {
+      let len = Object.keys(obj).length;
+      if (len == "3") {
+        if (obj.operation == null && obj.next == null) {
+          return obj.total;
+        } else if (obj.operation && obj.next == null) {
+          return obj.total + " " + obj.operation;
+        } else if (obj.operation && obj.next && obj.total) {
+          return obj.total + " " + obj.operation + " " + obj.next;
+        }
       }
-        
-      );
-    }
-    console.log(e.target.innerText)
-  };
+      if (len == "2") {
+        return obj.next;
+      }
+}
+  }
 
   return (
     <div className="grid-item-input value-input">
-    {props.result.total}{props.result.operation }{props.result.next} 
+      {show() || 0}
     </div>
     
   );
 }
-
-
-
 export default DisplayInput;
